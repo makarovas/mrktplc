ac04e1b09ca20f411e3c0c3ee616e792abcdad5f
Aleksandr Makarov <makarofalex@gmail.com>
2021-04-16 12:41:03 +0300

    add cors

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 import express from "express";
 import { readdirSync } from "fs";
+import mongoose from "mongoose";
 require("dotenv").config();
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// middleware
+app.use(cors());
+app.use(morgan("dev"));
+
+// route
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
 app.listen(PORT, () => console.log(`run on ${PORT}`));
 
+//export
 module.exports = {
   express,
 };
